bac2c1a73caa52685172c2c6127fe46b947bb56b
dyllew3 <dylan.lewis3@gmail.com>
2017-03-21 16:24:52 +0000

    FIxed auth and admin stuff

--- Online-Booking-System/app/controllers/welcome_controller.rb
+++ Online-Booking-System/app/controllers/welcome_controller.rb
@@ -2,6 +2,32 @@ class WelcomeController < ApplicationController
 	 
 	def index
 	end
+	def new
+		
+	   @user = User.find_by(id:session[:user_id])
+	   if @user == nil
+		redirect_to login_url
+	   elsif @user.authenicated
+				
+        else
+		redirect_to root_url
+	   end
+
+	end
+	def add_admin
+		if params["admins"] != nil 
+			@emails = params["admins"].keys
+	   		#flash[:notice] = params["approved"].keys
+	   		@emails.each do |u|
+			
+				@user = User.find_by(email: u)
+				@user.up_admin unless params["admins"][u.to_s].to_i == 0 
+							
+			end 
+		end
+		redirect_to '/admins/add' 
+	end
+	
 	def approve
 	   @test = Parent.new
 	   @users = []
@@ -28,6 +54,20 @@ class WelcomeController < ApplicationController
 		end
 		redirect_to approve_url
 	end
+	private 
+	
+	def non_admin
+		@all = User.all
+	    @result = []
+	    @all.each do |u|
+			if !u.admin?
+		   		@result << u
+			end		
+	    end
+        return @result
+
+	end
+	
 	def not_authenticated
 	    @all = User.all
 	    @result = []
@@ -38,5 +78,5 @@ class WelcomeController < ApplicationController
 	    end
         return @result
 	end
-	helper_method :not_authenticated
+	helper_method :not_authenticated, :non_admin
 end
