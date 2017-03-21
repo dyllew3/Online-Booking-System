531d293d4fa94865c30c4ba8b5a2cbe029e4921c
tmcgahern <mcgahert@tcd.ie>
2017-03-21 22:44:03 +0000

    Added children names to application activity forms

--- Online-Booking-System/app/controllers/application_controller.rb
+++ Online-Booking-System/app/controllers/application_controller.rb
@@ -2,8 +2,23 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  helper_method :current_user
   def not_found
 	redirect_to root_url
 	end
+	
+  private
+
+  def current_user
+    return unless session[:user_id]
+    @current_user ||= User.find(session[:user_id])
+  end
+
+  def logged_in?
+    !!session[:user_id]
+  end
+  
+  
 
 end
--- Online-Booking-System/app/models/student.rb
+++ Online-Booking-System/app/models/student.rb
@@ -2,4 +2,9 @@ class Student < ActiveRecord::Base
   belongs_to :parent
   has_one :StudentClass
   has_many :applications, dependent: :destroy
+  
+  
+  def first_last_name
+		"#{first_name} #{last_name}"
+	end
 end
