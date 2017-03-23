ea0930294bf9a2c0fc30e44e882dd4f81fa33a75
tmcgahern <mcgahert@tcd.ie>
2017-03-23 17:55:25 +0000

    Fixed the error with the seed file

--- Online-Booking-System/db/schema.rb
+++ Online-Booking-System/db/schema.rb
@@ -11,22 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170321134228) do
-
-  create_table "activities", force: :cascade do |t|
-    t.integer  "ResponsibleTeacher"
-    t.text     "Description"
-    t.integer  "StartClassSuitability"
-    t.integer  "EndClassSuitability"
-    t.date     "StartDate"
-    t.date     "EndDate"
-    t.time     "StartTime"
-    t.time     "EndTime"
-    t.integer  "NoOfChildren"
-    t.datetime "created_at",            null: false
-    t.datetime "updated_at",            null: false
-    t.string   "ActivityName"
-  end
+ActiveRecord::Schema.define(version: 20170321143359) do
 
   create_table "activity_dates", force: :cascade do |t|
     t.integer  "activity_id"
