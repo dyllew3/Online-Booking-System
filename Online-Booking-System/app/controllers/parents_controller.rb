61f814a8b328e46af37dd426e845e3d903f99627
dyllew3 <dylan.lewis3@gmail.com>
2017-04-02 22:23:00 +0100

    Fixed error where it was not possible to update parent

--- Online-Booking-System/app/controllers/parents_controller.rb
+++ Online-Booking-System/app/controllers/parents_controller.rb
@@ -31,13 +31,13 @@ class ParentsController < ApplicationController
 
   # GET /parents/1/edit
   def edit
-	@cur_parent = nil	
+	@parent = nil	
 	path = request.original_url.split('/')[-2]
-	@cur_parent =Parent.find_by(id: path.to_i)
-	@user = User.find_by(userable_id: @cur_parent.id,userable_type:"Parent")
+	@parent =Parent.find_by(id: path.to_i)
+	@user = User.find_by(userable_id: @parent.id,userable_type:"Parent")
 	redirect_to root_url unless !@user.nil?
 	#@parent.user << @user
-			redirect_to login_url unless !current_user.nil? and current_user.authenticated? and (@cur_parent.id == current_user.id or current_user.admin?) 
+			redirect_to login_url unless !current_user.nil? and current_user.authenticated? and (@parent.id == current_user.id or current_user.admin?) 
 
   end
 
@@ -74,7 +74,7 @@ class ParentsController < ApplicationController
   # PATCH/PUT /parents/1.json
   def update
 	path = request.original_url.split('/')[-1]
-	@cur_parent =Parent.find_by(id: path.to_i)
+	@parent =Parent.find_by(id: path.to_i)
 	if !current_user.nil? and current_user.authenticated? and (@parent.id == current_user.id or current_user.admin?) 
    respond_to do |format|
       if @parent.update(parent_params) 
